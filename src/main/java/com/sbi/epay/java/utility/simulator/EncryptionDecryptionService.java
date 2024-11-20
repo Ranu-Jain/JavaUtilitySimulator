8051955c5fce6b72b797fb87aff1f1e4167e7b9c
RJ2610 <ranu.jain@joshsoftware.com>
2024-11-21 00:31:12 +0530

    RJ | Initial commit

--- src/main/java/com/sbi/epay/java/utility/simulator/EncryptionDecryptionService.java
+++ src/main/java/com/sbi/epay/java/utility/simulator/EncryptionDecryptionService.java
@@ -1,35 +1,55 @@
 package com.sbi.epay.java.utility.simulator;
 
-import com.sbi.epay.encryptdecrypt.constant.EncryptionDecryptionConstants;
-import com.sbi.epay.encryptdecrypt.exception.EncryptionDecryptionException;
 import com.sbi.epay.encryptdecrypt.service.DecryptionService;
 import com.sbi.epay.encryptdecrypt.service.EncryptionService;
 import com.sbi.epay.encryptdecrypt.service.KeyGeneratorService;
-import com.sbi.epay.encryptdecrypt.util.EncryptionDecryptionAlgo;
-import com.sbi.epay.encryptdecrypt.util.GCMIvLength;
-import com.sbi.epay.encryptdecrypt.util.GCMTagLength;
-import com.sbi.epay.encryptdecrypt.util.KeyGenerationAlgo;
-import com.sbi.epay.encryptdecrypt.util.SecretKeyLength;
+import com.sbi.epay.encryptdecrypt.service.KeyProviderService;
+import com.sbi.epay.encryptdecrypt.util.*;
 
 import javax.crypto.SecretKey;
-import java.text.MessageFormat;
 
 public class EncryptionDecryptionService {
     public static void main(String[] args) throws Exception {
         try {
-            KeyGeneratorService keyGeneratorService = new KeyGeneratorService();
-            String s = keyGeneratorService.generateKeyByDefaultAlgo(SecretKeyLength.AES_128);
-            System.out.println(s);
-            SecretKey secretKey = keyGeneratorService.getSecretKey(SecretKeyLength.AES_128, KeyGenerationAlgo.AES);
-            System.out.println(secretKey);
             EncryptionService encryptionService = new EncryptionService();
-            byte[] bytes = encryptionService.encryptValue(secretKey, "{name : Ranu}", EncryptionDecryptionAlgo.AES_GCM_NO_PADDING, GCMIvLength.MAXIMUM, GCMTagLength.STANDARD);
-            System.out.println(bytes);
             DecryptionService decryptionService = new DecryptionService();
-            String decryptionValue = decryptionService.decryptValue(bytes, secretKey, EncryptionDecryptionAlgo.AES_GCM_NO_PADDING, GCMIvLength.MAXIMUM, GCMTagLength.STANDARD);
-            System.out.println(decryptionValue);
+            KeyGeneratorService keyGeneratorService = new KeyGeneratorService();
+            KeyProviderService keyProviderService = new KeyProviderService();
+
+            //Step 1 : Key generator
+            String aek = keyGeneratorService.generateKeyByAlgo(SecretKeyLength.AES_128, KeyGenerationAlgo.AES);
+            System.out.println("aek: " + aek);
+            String kek = keyGeneratorService.generateKeyByAlgo(SecretKeyLength.AES_128, KeyGenerationAlgo.AES);
+            System.out.println("kek: " + kek);
+            String mek = keyGeneratorService.generateKeyByAlgo(SecretKeyLength.AES_128, KeyGenerationAlgo.AES);
+            System.out.println("mek: " + mek);
+
+            //Step 2 : Key Encryption
+            String encryptedKeK = encryptionService.encryptSecretKey(kek, aek, EncryptionDecryptionAlgo.AES_GCM_NO_PADDING, GCMIvLength.MAXIMUM, GCMTagLength.STANDARD);
+            System.out.println("encryptedKeK : " + encryptedKeK);
+            String encryptedMeK = encryptionService.encryptSecretKey(mek, kek, EncryptionDecryptionAlgo.AES_GCM_NO_PADDING, GCMIvLength.MAXIMUM, GCMTagLength.STANDARD);
+            System.out.println("encryptedMeK : " + encryptedMeK);
+
+            //Step : Key Decryption
+            kek = decryptionService.decryptKey(encryptedKeK, aek, EncryptionDecryptionAlgo.AES_GCM_NO_PADDING, GCMIvLength.MAXIMUM, GCMTagLength.STANDARD);
+            System.out.println("decryptedKek : " + kek);
+            mek = decryptionService.decryptKey(encryptedMeK, kek, EncryptionDecryptionAlgo.AES_GCM_NO_PADDING, GCMIvLength.MAXIMUM, GCMTagLength.STANDARD);
+            System.out.println("decryptedMek : " + mek);
+
+            //Step 3 : Key Provider
+            SecretKey mekSecretKey = keyProviderService.getDecryptedMEK(encryptedMeK, encryptedKeK, aek, EncryptionDecryptionAlgo.AES_GCM_NO_PADDING, GCMIvLength.MAXIMUM, GCMTagLength.STANDARD);
+            System.out.println("mekSecretKey : " + mekSecretKey);
+
+            //Step 4 : Value Encryption by Key
+            String json = "{\"name\":\"ranjan\",\"phoneNumber\":\"9818809058\",\"email\":\"ran@gmail.com\",\"gstIn\":\"27AAACS8577K2ZO\"}";
+            byte[] encryptedValue = encryptionService.encryptValue(mekSecretKey, json, EncryptionDecryptionAlgo.AES_GCM_NO_PADDING, GCMIvLength.MAXIMUM, GCMTagLength.STANDARD);
+            System.out.println("encryptedValue : " + encryptedValue);
+
+            //Step 5 : Value decryption by Key
+            String decryptionValue = decryptionService.decryptValue(encryptedValue, mekSecretKey, EncryptionDecryptionAlgo.AES_GCM_NO_PADDING, GCMIvLength.MAXIMUM, GCMTagLength.STANDARD);
+            System.out.println("decryptionValue : " + decryptionValue);
         } catch (NullPointerException e) {
-            throw new Exception(e.getMessage() ,e);
+            throw new Exception(e.getMessage(), e);
         }
     }
 }
